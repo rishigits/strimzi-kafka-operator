f0d329e8bb3f2a37a9f9cc05820124308618a448
rishigits <rishigits@users.noreply.github.com>
2023-09-13 12:38:45 -0400

    Update build_keycloak_opa-s390x.sh
    Signed-off-by: rishigits <rishigits@users.noreply.github.com>

--- .jenkins/scripts/build_keycloak_opa-s390x.sh
+++ .jenkins/scripts/build_keycloak_opa-s390x.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#eval $(minikube docker-env)
+eval $(minikube docker-env)
 
 sed -i 's#kubernetes.yml#kubernetes.yml | sed "s\#Always\#IfNotPresent\#g"#g' systemtest/src/test/resources/oauth2/prepare_keycloak_operator.sh
 sed -i 's#kubectl delete -n ${KEYCLOAK_OPERATOR_NAMESPACE} -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/kubernetes.yml#curl -s https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/kubernetes.yml | sed "s\#Always\#IfNotPresent\#g" | kubectl delete -n \${KEYCLOAK_OPERATOR_NAMESPACE} -f -#g' systemtest/src/test/resources/oauth2/teardown_keycloak_operator.sh
